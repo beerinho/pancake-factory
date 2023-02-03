d16316c0b393e4579678fdbc3e054b89a30eb3fc
BEERINHO <danieljamesbeer@gmail.com>
2023-02-03 14:37:45 +0100

    Add Ferris

--- app/modifiers/create-scene.js
+++ app/modifiers/create-scene.js
@@ -1,9 +1,7 @@
 import { modifier } from 'ember-modifier';
 import * as THREE from 'three';
-import { AxisGridHelper } from '../utils/axis-grid-helper';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const gui = new GUI();
 
 // renderer
 let renderer = null;
@@ -17,9 +15,9 @@ const aspect = 2; // the canvas default
 const near = 0.1;
 const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(-10, 7.5, 20);
+camera.position.set(1, 3, 5);
 camera.up.set(0, 1, 0);
-camera.lookAt(0, 0, 0);
+camera.lookAt(0, 0.25, 0);
 const cameraHolder = new THREE.Object3D();
 cameraHolder.add(camera);
 
@@ -31,11 +29,14 @@ scene.add(cameraHolder);
 // lights
 {
   const color = 0xffffff;
-  const intensity = 1;
+  const intensity = 120;
   const light = new THREE.DirectionalLight(color, intensity);
   light.position.set(-3, 3, 2);
   light.castShadow = true;
   scene.add(light);
+
+  const light2 = new THREE.AmbientLight(color, 10);
+  scene.add(light2);
 }
 
 function resizeRendererToDisplaySize(renderer) {
@@ -68,143 +69,26 @@ const render = (time) => {
   requestAnimationFrame(render);
 };
 
-function createCar() {
-  console.log('creating car')
-  const car = new THREE.Group();
-  scene.add(car);
+const loader = new GLTFLoader();
 
-  const bodyGeometry = new THREE.BoxGeometry(10, 2, 5);
-  const bodyMaterial = new THREE.MeshLambertMaterial({
-    color: 0xce2f2d,
-    emissive: 0xce2f2d,
-    emissiveIntensity: 0.3,
-  });
-  const bodyMesh = new THREE.Mesh(bodyGeometry, bodyMaterial);
-  bodyMesh.castShadow = true;
-  bodyMesh.receiveShadow = true;
-  car.add(bodyMesh);
+loader.load('/models/ferris/ferris3d_v1.0.gltf', function (gltf) {
 
-  // create cabin
-  const cabinY = 1.9;
-  const cabinGeometry = new THREE.BoxGeometry(5, cabinY, 4.5);
-  const carFrontTexture = getCarFrontTexture();
+  scene.add(gltf.scene);
 
-  const carBackTexture = getCarFrontTexture();
+}, undefined, function (error) {
 
-  const carRightSideTexture = getCarSideTexture();
+  console.error(error);
 
-  const carLeftSideTexture = getCarSideTexture();
-  carLeftSideTexture.center = new THREE.Vector2(0.5, 0.5);
-  carLeftSideTexture.rotation = Math.PI;
-  carLeftSideTexture.flipY = false;
-
-  const cabin = new THREE.Mesh(cabinGeometry, [
-    new THREE.MeshLambertMaterial({
-      map: carFrontTexture,
-      emissive: 0xffffff,
-      emissiveMap: carFrontTexture,
-      emissiveIntensity: 0.3,
-    }),
-    new THREE.MeshLambertMaterial({
-      map: carBackTexture,
-      emissive: 0xffffff,
-      emissiveMap: carFrontTexture,
-      emissiveIntensity: 0.3,
-    }),
-    new THREE.MeshLambertMaterial({
-      color: 0xffffff,
-      emissive: 0xffffff,
-      emissiveIntensity: 0.3,
-    }), // top
-    new THREE.MeshLambertMaterial({
-      color: 0xffffff,
-      emissive: 0xffffff,
-      emissiveIntensity: 0.3,
-    }), // bottom
-    new THREE.MeshLambertMaterial({
-      map: carRightSideTexture,
-      emissive: 0xffffff,
-      emissiveMap: carRightSideTexture,
-      emissiveIntensity: 0.3,
-    }),
-    new THREE.MeshLambertMaterial({
-      map: carLeftSideTexture,
-      emissive: 0xffffff,
-      emissiveMap: carLeftSideTexture,
-      emissiveIntensity: 0.3,
-    }),
-  ]);
-  cabin.position.x = 1;
-  cabin.position.y = cabinY;
-  cabin.castShadow = true;
-  cabin.receiveShadow = true;
-  car.add(cabin);
-
-  // create tyres
-  createTyre(car, -3.5, 2.3);
-  createTyre(car, -3.5, -2.3);
-  createTyre(car, 3.5, 2.3);
-  createTyre(car, 3.5, -2.3);
-
-  makeAxisGrid(car, 'car', 25);
-}
-
-function getCarFrontTexture() {
-  const canvas = document.createElement('canvas');
-  canvas.width = 120;
-  canvas.height = 60;
-  const context = canvas.getContext('2d');
-
-  context.fillStyle = '#ffffff';
-  context.fillRect(0, 0, 120, 60);
-
-  context.fillStyle = '#000000';
-  context.fillRect(10, 10, 100, 50);
-
-  return new THREE.CanvasTexture(canvas);
-}
-
-function getCarSideTexture() {
-  const canvas = document.createElement('canvas');
-  canvas.width = 200;
-  canvas.height = 60;
-  const context = canvas.getContext('2d');
-
-  context.fillStyle = '#ffffff';
-  context.fillRect(0, 0, 200, 60);
-
-  context.fillStyle = '#000000';
-  context.fillRect(10, 10, 60, 50);
-  context.fillRect(80, 10, 110, 50);
-
-  return new THREE.CanvasTexture(canvas);
-}
-
-function createTyre(car, x, z) {
-  const tyreGeometry = new THREE.CylinderGeometry(1, 1, 0.75, 4);
-  const tyreMaterial = new THREE.MeshLambertMaterial({ color: 0x000000 });
-  const tyreMesh = new THREE.Mesh(tyreGeometry, tyreMaterial);
-  tyreMesh.position.y = -1;
-  tyreMesh.position.x = x;
-  tyreMesh.position.z = z;
-  tyreMesh.rotation.x = 1.57;
-  car.add(tyreMesh);
-  objects.push(tyreMesh);
-}
-
-function makeAxisGrid(node, label, units) {
-  const helper = new AxisGridHelper(node, units);
-  gui.add(helper, 'visible').name(label);
-}
+});
 
 export default modifier(function createScene(
   element
 ) {
   renderer = new THREE.WebGLRenderer({ canvas: element });
   renderer.shadowMap.enabled = true;
+  renderer.physicallyCorrectLights = true
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
   renderer.render(scene, camera);
-  createCar();
   render();
 }, { eager: false });
 
